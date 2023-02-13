164a8ee854e6eb9585a3728baa5af4c4cae8ccc2
Nevaden <aaron.k.dodson@gmail.com>
2023-02-13 00:31:42 -0500

    resolver initial setup/ split data in two parts for routing

--- src/app/resolver/data.resolver.ts
+++ src/app/resolver/data.resolver.ts
@@ -5,12 +5,27 @@ import {
   ActivatedRouteSnapshot
 } from '@angular/router';
 import { Observable, of } from 'rxjs';
+import { PackageOptions } from '../class/package-options';
 
 @Injectable({
   providedIn: 'root'
 })
-export class DataResolver implements Resolve<boolean> {
-  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return of(true);
+export class DataResolver implements Resolve<any> {
+  constructor(private dataService: PackageOptions){}
+  resolve(): Observable<any> {
+    const data = this.dataService.createDb().packageOptions;
+
+
+    // Divide the data into two parts
+    let firstHalf = data.slice(0, data.length / 2);
+    let secondHalf = data.slice(data.length / 2);
+
+    // Return the two parts as an observable
+    return new Observable((observer) => {
+      observer.next([firstHalf, secondHalf]);
+      observer.complete();
+    });
+
   }
 }
+
