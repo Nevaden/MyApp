5e1f9adca80f5a83bdd0a7d17dc88d5eaf59ac0b
Nevaden <aaron.k.dodson@gmail.com>
2023-02-13 00:59:11 -0500

    one page component / subscribe for resolver data

--- src/app/pages/one/one.component.ts
+++ src/app/pages/one/one.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { PackageOptions } from 'src/app/class/package-options';
 
 @Component({
   selector: 'app-one',
@@ -6,10 +8,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./one.component.scss']
 })
 export class OneComponent implements OnInit {
+  data!: any;
+  constructor(private route: ActivatedRoute, private dataService: PackageOptions) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.route.data.subscribe((data) => {
+      this.data = data['data'];
+    });
   }
 
 }
