2772bcb50b2c059b51b17215ffe800c65393e5c0
Nevaden <aaron.k.dodson@gmail.com>
2023-02-13 01:38:26 -0500

    buy/cancel button hiding but not correclty. need to fix only hide buy/ only show cancel

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PackageOptions } from 'src/app/class/package-options';
 
 @Component({
   selector: 'app-card',
@@ -9,10 +10,33 @@ export class CardComponent implements OnInit {
 
   @Input() item: any;
 
-  constructor() { }
+  constructor( public packageOptionsService: PackageOptions ) { }
 
   ngOnInit(): void {
    
   }
 
+  updateSelection(packageOption: any) {
+    if (packageOption) {
+      this.packageOptionsService.updateSelection(packageOption);
+    } else {
+      const selected = (element: { isSelected: any; }) => element.isSelected
+      const selectedOption = this.packageOptionsService.packageOptions.find(selected)
+      selectedOption!.isSelected = false;
+    }
+  }
+
+  getClasses(item: { isSelected: any; }) {
+    return {
+      [this.item.title]: true,
+      'selected': item.isSelected,
+      'not-selected': !item.isSelected && this.hasSelectedItem()
+    };
+  }
+  
+  hasSelectedItem(): boolean {
+    const selected = (element: { isSelected: any; }) => element.isSelected
+    return this.packageOptionsService.packageOptions.some(selected)
+  }
+  
 }
