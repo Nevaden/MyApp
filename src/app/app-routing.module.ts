f16618989bf5da871e1ee1ae3cf4176929199669
Nevaden <aaron.k.dodson@gmail.com>
2023-02-13 00:27:53 -0500

    routes setup/ base resolver direction /splitting data in parts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OneComponent } from './pages/one/one.component';
+import { TwoComponent } from './pages/two/two.component';
+import { DataResolver } from './resolver/data.resolver';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'one',
+    component: OneComponent,
+    resolve: {
+      data: DataResolver
+    },
+    data: {
+      part: 0
+    }
+
+  },
+  {
+    path: 'two',
+    component: TwoComponent,
+    resolve: {
+      data: DataResolver
+    },
+    data: {
+      part: 1
+    }
+
+  },
+  { path: '', redirectTo: '/one', pathMatch: 'full' },
+  { path: '**', component: OneComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
