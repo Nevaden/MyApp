85538a8c5936b21556e6d1d6b90e9bd0e8608243
Nevaden <aaron.k.dodson@gmail.com>
2023-02-13 00:16:50 -0500

    inital create of components/resolver/class/pagescomponents

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OneComponent } from './pages/one/one.component';
+import { TwoComponent } from './pages/two/two.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CardComponent } from './components/card/card.component';
+import { PackageOptionsComponent } from './class/package-options/package-options.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OneComponent,
+    TwoComponent,
+    HeaderComponent,
+    CardComponent,
+    PackageOptionsComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/class/package-options/package-options.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-package-options',
+  templateUrl: './package-options.component.html',
+  styleUrls: ['./package-options.component.scss']
+})
+export class PackageOptionsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
